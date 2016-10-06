2b2d13a6d38fb9336fda85e909dbf7668624557b
Matt Campbell <mattica@gmail.com>
2016-10-06 09:57:32 -0700

    first commit, basically a modification of OpenTK demo in BulletSharp

---
+++ BasicDemo/BasicDemo.cs
@@ -0,0 +1,189 @@
+﻿using System.Drawing;
+using BulletSharp;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Input;
+
+namespace BasicDemo
+{
+    class BasicDemo : GameWindow
+    {
+        Physics physics;
+        float angle = 0.0f;
+        float frameTime; int fps;
+
+        public BasicDemo()
+            : base(800, 600,
+            new GraphicsMode(), "BulletSharp OpenTK Demo")
+        {
+            VSync = VSyncMode.Off;
+            physics = new Physics();
+        }
+
+        protected override void OnLoad(System.EventArgs e)
+        {
+            GL.Enable(EnableCap.DepthTest);
+            GL.ClearColor(Color.MidnightBlue);
+
+            GL.Enable(EnableCap.ColorMaterial);
+            GL.Enable(EnableCap.Light0);
+            GL.Enable(EnableCap.Lighting);
+        }
+
+        protected override void OnUnload(System.EventArgs e)
+        {
+            physics.ExitPhysics();
+            base.OnUnload(e);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            physics.Update((float)e.Time);
+
+            KeyboardState state = OpenTK.Input.Keyboard.GetState();
+            if (state.IsKeyDown(Key.Escape) || state.IsKeyDown(Key.Q))
+            {
+                Exit();
+            }
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            frameTime += (float)e.Time;
+            fps++;
+            if (frameTime >= 1)
+            {
+                frameTime = 0;
+                Title = "BulletSharp OpenTK Demo, FPS = " + fps.ToString();
+                fps = 0;
+            }
+
+            GL.Viewport(0, 0, Width, Height);
+
+            float aspect_ratio = Width / (float)Height;
+            Matrix4 perspective = Matrix4.CreatePerspectiveFieldOfView(MathHelper.PiOver4, aspect_ratio, 0.1f, 100);
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadMatrix(ref perspective);
+
+            Matrix4 lookat = Matrix4.LookAt(new Vector3(10, 20, 30), Vector3.Zero, Vector3.UnitY);
+            GL.MatrixMode(MatrixMode.Modelview);
+
+            GL.Rotate(angle, 0.0f, 1.0f, 0.0f);
+            angle += (float)e.Time*100;
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            InitCube();
+
+            foreach (RigidBody body in physics.World.CollisionObjectArray)
+            {
+                var worldTransform = new Matrix4(body.MotionState.WorldTransform.M11,
+                    body.MotionState.WorldTransform.M12, body.MotionState.WorldTransform.M13,
+                    body.MotionState.WorldTransform.M14, body.MotionState.WorldTransform.M21,
+                    body.MotionState.WorldTransform.M22, body.MotionState.WorldTransform.M23,
+                    body.MotionState.WorldTransform.M24, body.MotionState.WorldTransform.M31,
+                    body.MotionState.WorldTransform.M32, body.MotionState.WorldTransform.M33,
+                    body.MotionState.WorldTransform.M34, body.MotionState.WorldTransform.M41,
+                    body.MotionState.WorldTransform.M42, body.MotionState.WorldTransform.M43,
+                    body.MotionState.WorldTransform.M44);
+                Matrix4 modelLookAt =  worldTransform * lookat;
+                GL.LoadMatrix(ref modelLookAt);
+
+                if ("Ground".Equals(body.UserObject))
+                {
+                    DrawCube(Color.Green, 50.0f);
+                    continue;
+                }
+
+                if (body.ActivationState == ActivationState.ActiveTag)
+                    DrawCube2(Color.Orange);
+                else
+                    DrawCube2(Color.Red);
+            }
+
+            UninitCube();
+
+            SwapBuffers();
+        }
+
+        private void DrawCube(Color color, float size)
+        {
+            GL.Begin(BeginMode.Quads);
+
+            GL.Color3(color);
+            GL.Vertex3(-size, -size, -size);
+            GL.Vertex3(-size, size, -size);
+            GL.Vertex3(size, size, -size);
+            GL.Vertex3(size, -size, -size);
+
+            GL.Vertex3(-size, -size, -size);
+            GL.Vertex3(size, -size, -size);
+            GL.Vertex3(size, -size, size);
+            GL.Vertex3(-size, -size, size);
+
+            GL.Vertex3(-size, -size, -size);
+            GL.Vertex3(-size, -size, size);
+            GL.Vertex3(-size, size, size);
+            GL.Vertex3(-size, size, -size);
+            
+            GL.Vertex3(-size, -size, size);
+            GL.Vertex3(size, -size, size);
+            GL.Vertex3(size, size, size);
+            GL.Vertex3(-size, size, size);
+
+            GL.Vertex3(-size, size, -size);
+            GL.Vertex3(-size, size, size);
+            GL.Vertex3(size, size, size);
+            GL.Vertex3(size, size, -size);
+
+            GL.Vertex3(size, -size, -size);
+            GL.Vertex3(size, size, -size);
+            GL.Vertex3(size, size, size);
+            GL.Vertex3(size, -size, size);
+
+            GL.End();
+        }
+
+        float[] vertices = new float[] {1,1,1,  -1,1,1,  -1,-1,1,  1,-1,1,
+            1,1,1,  1,-1,1,  1,-1,-1,  1,1,-1,
+            1,1,1,  1,1,-1,  -1,1,-1,  -1,1,1,
+            -1,1,1,  -1,1,-1,  -1,-1,-1,  -1,-1,1,
+            -1,-1,-1,  1,-1,-1,  1,-1,1,  -1,-1,1,
+            1,-1,-1,  -1,-1,-1,  -1,1,-1,  1,1,-1};
+
+        float[] normals = new float[] {0,0,1,  0,0,1,  0,0,1,  0,0,1,
+            1,0,0,  1,0,0,  1,0,0, 1,0,0,
+            0,1,0,  0,1,0,  0,1,0, 0,1,0,
+            -1,0,0,  -1,0,0, -1,0,0,  -1,0,0,
+            0,-1,0,  0,-1,0,  0,-1,0,  0,-1,0,
+            0,0,-1,  0,0,-1,  0,0,-1,  0,0,-1};
+
+        byte[] indices = {0,1,2,3,
+            4,5,6,7,
+            8,9,10,11,
+            12,13,14,15,
+            16,17,18,19,
+            20,21,22,23};
+
+        void InitCube()
+        {
+            GL.EnableClientState(ArrayCap.NormalArray);
+            GL.EnableClientState(ArrayCap.VertexArray);
+            GL.NormalPointer(NormalPointerType.Float, 0, normals);
+            GL.VertexPointer(3, VertexPointerType.Float, 0, vertices);
+        }
+
+        void UninitCube()
+        {
+            GL.DisableClientState(ArrayCap.VertexArray);
+            GL.DisableClientState(ArrayCap.NormalArray);
+        }
+
+        void DrawCube2(Color color)
+        {
+            GL.Color3(color);
+            GL.DrawElements(BeginMode.Quads, 24, DrawElementsType.UnsignedByte, indices);
+        }
+    }
+}
---
+++ BasicDemo/Physics.cs
@@ -0,0 +1,170 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using BulletSharp;
+using BulletSharp.Math;
+using OpenTK;
+using TVGL;
+using TVGL.IOFunctions;
+
+//using OpenTK.Graphics;
+
+namespace BasicDemo
+{
+    class Physics
+    {
+        ///create 125 (5x5x5) dynamic objects
+        const int ArraySizeX = 5, ArraySizeY = 5, ArraySizeZ = 5;
+
+        ///scaling of the objects (0.1 = 20 centimeter boxes )
+        const float StartPosX = -5;
+        const float StartPosY = -5;
+        const float StartPosZ = -3;
+
+        public DiscreteDynamicsWorld World { get; set; }
+        CollisionDispatcher dispatcher;
+        DbvtBroadphase broadphase;
+        List<CollisionShape> collisionShapes = new List<CollisionShape>();
+        CollisionConfiguration collisionConf;
+
+        public Physics()
+        {
+            // collision configuration contains default setup for memory, collision setup
+            collisionConf = new DefaultCollisionConfiguration();
+            dispatcher = new CollisionDispatcher(collisionConf);
+
+            broadphase = new DbvtBroadphase();
+            World = new DiscreteDynamicsWorld(dispatcher, broadphase, null, collisionConf);
+            World.Gravity = new BulletSharp.Math.Vector3(0, -10, 0);
+
+            // create the ground
+            CollisionShape groundShape = new BoxShape(50, 50, 50);
+            collisionShapes.Add(groundShape);
+            CollisionObject ground = LocalCreateRigidBody(0, Matrix.Translation(0, -50, 0), groundShape);
+            ground.UserObject = "Ground";
+
+            // create a few dynamic rigidbodies
+            const float mass = 1.0f;
+            var filename = "ABF.STL";
+            List<TessellatedSolid> ts;
+            using (var fileStream = File.OpenRead(filename))
+                ts = IO.Open(fileStream, filename);
+            var mesh = new TriangleMesh();
+            var ts0 = ts[0];
+            foreach (var f in ts0.Faces)
+            {
+                mesh.AddTriangle(
+                    new BulletSharp.Math.Vector3(f.Vertices[0].Position.Cast<float>().ToArray()),
+                    new BulletSharp.Math.Vector3(f.Vertices[1].Position.Cast<float>().ToArray()),
+                    new BulletSharp.Math.Vector3(f.Vertices[2].Position.Cast<float>().ToArray()));
+            }
+            CollisionShape colShape = new 
+            //CollisionShape colShape = new BoxShape(1);
+            collisionShapes.Add(colShape);
+            var localInertia = colShape.CalculateLocalInertia(mass);
+
+            var rbInfo = new RigidBodyConstructionInfo(mass, null, colShape, localInertia);
+
+            const float start_x = StartPosX - ArraySizeX / 2;
+            const float start_y = StartPosY;
+            const float start_z = StartPosZ - ArraySizeZ / 2;
+
+            int k, i, j;
+            for (k = 0; k < ArraySizeY; k++)
+            {
+                for (i = 0; i < ArraySizeX; i++)
+                {
+                    for (j = 0; j < ArraySizeZ; j++)
+                    {
+                        //Matrix4 startTransform = Matrix4.CreateTranslation(
+                        //    new OpenTK.Vector3(
+                        //        2 * i + start_x,
+                        //        2 * k + start_y,
+                        //        2 * j + start_z
+                        //        )
+                        //    );
+                        var startTransform = Matrix.Translation(
+                                2 * i + start_x,
+                                2 * k + start_y,
+                                2 * j + start_z
+                                );
+
+                        // using motionstate is recommended, it provides interpolation capabilities
+                        // and only synchronizes 'active' objects
+                        rbInfo.MotionState = new DefaultMotionState(startTransform);
+
+                        RigidBody body = new RigidBody(rbInfo);
+
+                        // make it drop from a height
+                        body.Translate(new BulletSharp.Math.Vector3(0, 20, 0));
+
+                        World.AddRigidBody(body);
+                    }
+                }
+            }
+
+            rbInfo.Dispose();
+        }
+
+        public virtual void Update(float elapsedTime)
+        {
+            World.StepSimulation(elapsedTime);
+        }
+
+        public void ExitPhysics()
+        {
+            //remove/dispose constraints
+            int i;
+            for (i = World.NumConstraints - 1; i >= 0; i--)
+            {
+                TypedConstraint constraint = World.GetConstraint(i);
+                World.RemoveConstraint(constraint);
+                constraint.Dispose();
+            }
+
+            //remove the rigidbodies from the dynamics world and delete them
+            for (i = World.NumCollisionObjects - 1; i >= 0; i--)
+            {
+                CollisionObject obj = World.CollisionObjectArray[i];
+                RigidBody body = obj as RigidBody;
+                if (body != null && body.MotionState != null)
+                {
+                    body.MotionState.Dispose();
+                }
+                World.RemoveCollisionObject(obj);
+                obj.Dispose();
+            }
+
+            //delete collision shapes
+            foreach (CollisionShape shape in collisionShapes)
+                shape.Dispose();
+            collisionShapes.Clear();
+
+            World.Dispose();
+            broadphase.Dispose();
+            if (dispatcher != null)
+            {
+                dispatcher.Dispose();
+            }
+            collisionConf.Dispose();
+        }
+
+        public RigidBody LocalCreateRigidBody(float mass, Matrix startTransform, CollisionShape shape)
+        {
+            bool isDynamic = (mass != 0.0f);
+
+            var localInertia = BulletSharp.Math.Vector3.Zero;
+            if (isDynamic)
+                shape.CalculateLocalInertia(mass, out localInertia);
+
+            DefaultMotionState myMotionState = new DefaultMotionState(startTransform);
+
+            RigidBodyConstructionInfo rbInfo = new RigidBodyConstructionInfo(mass, myMotionState, shape, localInertia);
+            RigidBody body = new RigidBody(rbInfo);
+
+            World.AddRigidBody(body);
+
+            return body;
+        }
+    }
+}
